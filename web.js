d5a40d36bc8eaffb6944c7eff0eb78a357e41003
Heiwad Osman <heiwad@fake.fake>
2013-09-20 00:55:20 -0400

    Testing facebook login

--- web.js
+++ web.js
@@ -1,19 +1,23 @@
 var express = require('express');
 var app = express();
-
+var fs = require('fs');
 
 app.use(express.logger());
 
 app.get('/', function (request, response) {
-    response.send('Hello World!');
+
+    fs.readFile('./index.html', function (err, data) {
+	if (!err) response.send(data.toString());
+    });
+});
+
+app.get('/channel.html', function (request, response) {
+    fs.readFile('./channel.html', function (err, data) {
+	if (!err) response.send(data.toString());
+    });
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function () {
     console.log('Listening on ' + port);
 });
-
-
-
-
-
