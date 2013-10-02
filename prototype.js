72fe0a4ded724a35689b2d20ff071ad308a865ac
Heiwad <heiwad@fake.fake>
2013-10-02 19:58:42 +0000

    Commit before removing dead code just in case I need some of it later

--- prototype.js
+++ prototype.js
@@ -63,37 +63,170 @@ var facebookQueryBuilder = function ( token, user, target, fields) {
 };
 
 
-var requestHandler = function (error, response, body) {
+var fetchFriendsDevices = function (cb, user, token) {
 
-  if (!error && response.statusCode == 200) {
-    console.log(body);
-  }
+    token = token || getAuthToken();
+    user = user || 'me';
+    cb = cb || debugHandler;
+
+    var target = 'friends';
+    var fields = 'id,name,devices,picture.type(normal)';
+
+    var options = facebookQueryBuilder(token, user, target, fields);
+
+
+    var reduceDevices = function (output, friend) {
+
+	/* TODO:May want to eliminate use of 'contains' on the arrays if server load becomes a problem - or allow client side parsing of the data*/
+
+	if (!friend) return output;
+
+		output.friends.push(friend); /*this data i will be needed for rendering client side*/
+
+	if (!friend.devices) return output;
+
+
+	for (var i = 0; i < friend.devices.length; i++) {
+
+	    if (friend.devices[i].hasOwnProperty('os')) {
+		if (!output.os) output.os = {};
+
+		if (!output.os.hasOwnProperty(friend.devices[i].os)) {
+		    output.os[friend.devices[i].os] = {};
+		    output.os[friend.devices[i].os].count = 0;
+		    output.os[friend.devices[i].os].friends = [];
+		}
+
+		if (!uu.contains(output.os[friend.devices[i].os].friends, friend.id)) {
+		    output.os[friend.devices[i].os].count += 1;
+		    output.os[friend.devices[i].os].friends.push(friend.id);
+		}
+
+	    }
+
+	    if (friend.devices[i].hasOwnProperty('hardware')) {
+
+		if (!output.hardware) output.hardware = {};
+
+		if (!output.hardware[friend.devices[i].hardware]) {
+		    output.hardware[friend.devices[i].hardware] = {};
+		    output.hardware[friend.devices[i].hardware].count = 0;
+		    output.hardware[friend.devices[i].hardware].friends = [];
+		}
+		if (!uu.contains(output.hardware[friend.devices[i].hardware].friends, friend.id)) {
+		    output.hardware[friend.devices[i].hardware].count+=1;
+		    output.hardware[friend.devices[i].hardware].friends.push(friend.id);
+		}
+	    }
+	}
+
+	return output;
+    };
+
+    var reformatData = function (error, data) {
+	save(data, 'friends');
+	var reducedData = uu.reduce(data, reduceDevices, { friends:[], os:{}});
+	cb(null, reducedData);
+    };
+
+    pagedFacebookGet(options, reformatData);
+
 };
 
-var handleFriends = function (error, friends) {
 
-    if (error) { log(error); return;}
-    if (friends) {
-	var output = '';
-	for (var friend in friends) output += friends[friend].name + ' : ' +friends[friend].id +'\n';
-	log(output);
-	save(friends,'friends');
-	console.log('Got %d friends', friends.length);
+var pagedFacebookGet = function (options, cb, progresscb, maxPages) {
+
+    maxPages = maxPages || 5;
+    var pageCount = 1;
+    save(options, 'options');
+
+    var apiResults = [];
+
+    function fetch_more (error, response, body) {
+
+
+	if (!error && response.statusCode == 200) {
+
+	    try {
+
+		var parsedBody = JSON.parse(body);
+		var page = parsedBody.data;
+		var hasNext = parsedBody.paging.hasOwnProperty('next');
+
+		if (page.length > 0 ) {
+		    apiResults = apiResults.concat(page);
+		    if (typeof progresscb ==='function') progresscb(page);
+		}
+
+		if (hasNext && pageCount < maxPages) {
+		    pageCount++;
+		    request(parsedBody.paging.next, fetch_more);
+
+		} else {
+
+		    log('Done downloading. Received ' + apiResults.length + ' records. Calling completion callback...');
+		    cb(null, apiResults);
+		}
+
+
+	    } catch (e) {
+		log('Error parsing server response! ' + e);
+		save(e, 'lasterror');
+		save(body,'lastbody');
+		save(response,'lastresponse');
+		cb(e, apiResults); /*returns error and partial results to callback*/
+	    }
+
+
+	} else if (error){
+	    log('Error fetching data from facebook API: ' + error);
+	    save(error, 'lasterror');
+	    cb(error, null);
+	} else if (response.statusCode != 200) {
+	    log('Unexpected server response code when fetching data: ' + response.statusCode);
+	    save(response, 'lastresponse');
+	    save(body, 'lastbody');
+	    cb(error, null);
+	}
+
     }
 
+    log('Fetching data from facebook...');
+    request(options, fetch_more);
 };
 
-var handlePosts = function (error, posts) {
 
-    if (error) {log(error); return; }
-    else {
-	getAppsFromPosts(posts, function (error, apps) {
-	    if (error) log(error);
-	    else log('got apps'); save(apps,'apps');
-	});
+
+//Interactive Test Functions. Consider moving them to the client
+var findAppleAndAndroidOwners = function () {
+
+    for (var x in global._data.lastresult.os.iOS.friends) {
+	if (uu.contains(global._data.lastresult.os.Android.friends, global._data.lastresult.os.iOS.friends[x])){
+	    var bothOs = uu.where(global._data.lastresult.friends, {id: global._data.lastresult.os.iOS.friends[x]});
+	    for (x in bothOs) log(bothOs[x]);
+	}
 
     }
 };
+
+var findUnknownAppleDeviceOwners = function (friends) {
+
+    for (var ii = 0 ; ii < friends.length; ii ++) {
+	if (friends[ii].hasOwnProperty('devices')) {
+	    for (var jj = 0; jj < friends[ii].devices.length; jj++) {
+		if (friends[ii].devices[jj].hasOwnProperty('os') &&
+		    !friends[ii].devices[jj].hasOwnProperty('hardware') &&
+		    friends[ii].devices[jj].os === 'iOS') {
+		    log(friends[ii].name);
+		    }
+	    }
+	}
+    }
+};
+
+
+/*Old Code. This is too slow to be a viable way to fetch the app list. Rework this to use batched API calls that query for apps directly instead*/
+
 
 var getAppsFromPosts = function (posts, cb) {
 
@@ -133,26 +266,81 @@ var getAppsFromPosts = function (posts, cb) {
     cb(null, applications);
 };
 
+var getFriendsApps = function (token, friends, cb) {
+    //this is too slow.it appears to fetch in series. it took 40 minutes. This needs to be rewritten to use the facebook batch api - http://bit.ly/GzM8q9
+
+    token = token || getAuthToken();
+    friends = friends || _data.friends;
+    cb = cb || debugHandler;
+
+    var MAX_DOWNLOADS = 300;
+
+    var iterator = function (friend, resultcb) {
+	var filterPosts = function (error, posts) {
+	    if (error) resultcb(error, posts);
+	    else {
+
+		getAppsFromPosts(posts, resultcb);
+	    }
+	};
+
+	fetchPosts(filterPosts, friend.id, 'application');
+    };
+
+
+    async.mapLimit(friends, MAX_DOWNLOADS, iterator, cb);
+};
+
+
+/*Junk code - these functions aren't ready*/
+
+
+var handleFriends = function (error, friends) {
+
+    if (error) { log(error); return;}
+    if (friends) {
+	var output = '';
+	for (var friend in friends) output += friends[friend].name + ' : ' +friends[friend].id +'\n';
+	log(output);
+	save(friends,'friends');
+	console.log('Got %d friends', friends.length);
+    }
+
+};
+
+var handlePosts = function (error, posts) {
+
+    if (error) {log(error); return; }
+    else {
+	getAppsFromPosts(posts, function (error, apps) {
+	    if (error) log(error);
+	    else log('got apps'); save(apps,'apps');
+	});
+
+    }
+};
+
+
 var fetchFriends = function (cb) {
 
     var target = 'friends';
     var token = getAuthToken();
     var user = 'me';
-    cb = cb || handleFriends;
+    cb = cb || debugHandler;
 
     var options = facebookQueryBuilder(token, user, target );
     pagedFacebookGet(options, cb);
 
 };
 
-var fetchPosts = function (cb, user, fields) {
+var fetchPosts = function (cb, user, token, fields) {
 
     user = user || 'me';
-    cb = cb || handlePosts;
+    cb = cb || debugHandler;
     fields = fields || 'application';
 
     var target = 'posts';
-    var token = getAuthToken();
+    token = token || getAuthToken();
     var options = facebookQueryBuilder(token, user, target, fields);
 
 
@@ -167,119 +355,6 @@ var fetchPosts = function (cb, user, fields) {
 
 
     pagedFacebookGet(options, appendUserBeforeCb);
-
-
-};
-
-var pagedFacebookGet = function (options, cb, progresscb) {
-
-    var maxPages = 5;
-    var pageCount = 1;
-    log('Initiating paged fetch from facebook');
-    save(options, 'options');
-    var apiResults = [];
-
-    function fetch_more (error, response, body) {
-
-
-	if (!error && response.statusCode == 200) {
-	    log('Page of data successfully retreived from API');
-	    try {
-
-		var parsedBody = JSON.parse(body);
-
-		var page = parsedBody.data;
-		var hasNext = parsedBody.paging.hasOwnProperty('next');
-
-		if (page.length > 0 ) {
-		    apiResults = apiResults.concat(page);
-		    if (typeof progresscb ==='function') progresscb(page);
-		}
-
-		if (hasNext && pageCount < maxPages) {
-		    pageCount++;
-		    log('Scheduling download of additional pages of data');
-		    request(parsedBody.paging.next, fetch_more);
-
-		} else {
-
-		    log('Done downloading. Received ' + apiResults.length + ' records. Calling completion callback...');
-		    cb(null, apiResults);
-		}
-
-
-	    } catch (e) {
-		log('Error parsing server response! ' + e);
-		save(e, 'lasterror');
-		save(body,'lastbody');
-		save(response,'lastresponse');
-		cb(e, apiResults); /*returns error and partial results to callback*/
-	    }
-
-
-	} else if (error){
-	    log('Error fetching data from facebook API: ' + error);
-	    save(error, 'lasterror');
-	    cb(error, null);
-	} else if (response.statusCode != 200) {
-	    log('Unexpected server response code when fetching data: ' + response.statusCode);
-	    save(response, 'lastresponse');
-	    save(body, 'lastbody');
-	    cb(error, null);
-	}
-
-    }
-
-    log('Scheduling intial fetch of data');
-    request(options, fetch_more);
-};
-
-
-var handleFriendsApps = function ( error, friendsApps) {
-
-    if (error) {
-
-	log(error);
-	return;
-    } else {
-	save(friendsApps, 'friendsApps');
-    }
-
-};
-
-var getFriendsApps = function (token, friends, cb) {
-
-    //operates on a friends list
-    //applies each friend to a curry'ed version of pagedGet/app list
-    //returns final list of apps
-
-    token = token || getAuthToken();
-    friends = friends || _data.friends;
-    cb = cb || handleFriendsApps;
-
-    //need a function that takes in a user, and underscore callback. the function queries facebook for the post history of the user, then filters the posts down to generate a list of apps before calling the underscore callback
-
-    var MAX_DOWNLOADS = 300;
-
-
-    var iterator = function (friend, resultcb) {
-
-	var filterPosts = function (error, posts) {
-
-	    if (error) resultcb(error, posts);
-	    else {
-
-		getAppsFromPosts(posts, resultcb);
-		}
-	};
-
-
-	fetchPosts(filterPosts, friend.id, 'application');
-
-    };
-
-
-    async.mapLimit(friends, MAX_DOWNLOADS, iterator, cb);
 
 
 };
